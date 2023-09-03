1ec55e4f26d894861dd4b898d95bab85d26b6374
nutcas3 <mauricee423@gmail.com>
2023-09-03 14:04:50 +0300

    🎨: finished the inspiration

---
+++ src/config.js
@@ -0,0 +1,29 @@
+export default {
+	metadata: {
+		author: "Quentin Poterek",
+		summary: "Quentin Poterek's personal portfolio",
+		category: "Portfolio",
+		keywords: ["Portfolio", "Blog", "Quentin Poterek", "Poterek", "Géomatique", "Geomatics", "Data science", "Sciences des données","Map", "Carte", "Programming", "Programmation", "Remote sensing","Télédétection", "Deep learning", "Apprentissage profond"],
+		github: "https://github.com/poterekq",
+		linkedin: "https://www.linkedin.com/in/quentin-poterek/",
+	},
+	pages: [
+		{
+			text: "Home",
+			href: "/",
+		},
+		{
+			text: "About",
+			href: "/about",
+		},
+		{
+			text: "Work",
+			href: "/work",
+		},
+
+		{
+			text: "Contact",
+			href: "/contact",
+		},
+	]
+}
